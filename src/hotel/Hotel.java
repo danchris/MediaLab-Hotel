599ac1cb653731e3f25944c99d1de7593c92172e
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2018-11-17 02:57:14 +0200

    Init

---
+++ src/hotel/Hotel.java
@@ -0,0 +1,15 @@
+package hotel;
+
+import javafx.application.Application;
+
+
+/*
+ * @author Daniel Christodoulopoulos
+ */
+public class Hotel extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
